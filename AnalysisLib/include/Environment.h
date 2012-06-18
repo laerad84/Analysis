bea9e822dc994fc65bab9b823044d5fb652dc451
Lee Jong-won <jwlee@laerad-00.laeradlocal>
2012-06-18 17:45:33 +0900

    Add GetEnvironment function

---
+++ AnalysisLib/include/Environment.h
@@ -0,0 +1,14 @@
+#ifndef ENVIRONMENT__H__
+#define ENVIRONMENT__H__
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+
+std::string sumFileDir;
+std::string convFileDir;
+std::string rawFileDir;
+std::string expcalFileDir;
+std::string simcalFileDir;
+unsigned int GetEnvironment();
+
+#endif
---
+++ AnalysisLib/src/Environment.cc
@@ -0,0 +1,31 @@
+#include "Environment.h"
+
+unsigned int GetEnvironment(){
+  unsigned int Flag = 0;  
+  sumFileDir  = std::getenv("SUMUPFILEDIR");
+  convFileDir = std::getenv("CONVFILEDIR");
+  rawFileDir  = std::getenv("RAWFILEDIR");
+  expcalFileDir  = std::getenv("EXPCALFILEDIR");
+  simcalFileDir  = std::getenv("SIMCALFILEDIR"):
+  if( sumFileDir.size() == 0){
+    std::cerr << "There is no Definition : SUMUPFILEDIR" << std::endl;
+    Flag |= 1;
+  }
+  if( convFileDir.size() == 0 ){
+    std::cerr << "There is no Definition : CONVFILEDIR" << std::endl;
+    Flag |= 2;
+  }
+  if( rawFileDir.size() == 0 ){
+    std::cerr << "There is no Definition : RAWFILEDIR" << std::endl;
+    Flag |=4;
+  }
+  if( expcalFileDir.size() == 0 ){
+    std::cerr << "There is no Definition : EXPCALFILEDIR" << std::endl; 
+    Flag |= 8; 
+  }
+  if( simcalFileDir.size() == 0 ){
+    std::cerr << "There is no Definition : SIMCALFILEDIR" << std::endl;
+    Flag |= 16;
+  }
+  return Flag;
+}
