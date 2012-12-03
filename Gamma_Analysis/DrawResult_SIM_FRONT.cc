26ced6f6767b99a2e5c7c77582eda5765cb148a0
jwlee <laerad84.gmail.com>
2012-12-03 17:05:49 +0900

    Update

--- Gamma_Analysis/DrawResult_SIM_FRONT.cc
+++ Gamma_Analysis/DrawResult_SIM_FRONT.cc
@@ -101,8 +101,7 @@ int main( int argc, char** argv ){
       if( reader->ClusterR[clusterIndex] > 500 ){ continue; }
       if( reader->ClusterR[clusterIndex] < 250 ){ continue; }
       for( int crystalIndex  = 0; crystalIndex < reader->nCrystal[ clusterIndex ]; crystalIndex++){       
-	if( reader->CrystalEnergy[clusterIndex][crystalIndex] <= 3 ) { continue; }
-	if( reader->CrystalEnergy[clusterIndex][crystalIndex] >= 400){ continue; }
+	if( reader->CrystalEnergy[clusterIndex][crystalIndex] <= 3 ) { continue; }	
 	
 	Double_t RinCluster = reader->CrystalR[ clusterIndex][crystalIndex]*TMath::Cos( reader->CrystalPhi[clusterIndex][crystalIndex]);
 	Double_t DinCluster = reader->CrystalR[ clusterIndex][crystalIndex]*TMath::Sin( reader->CrystalPhi[clusterIndex][crystalIndex]);
