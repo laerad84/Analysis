bebba9afeecc14f6795345b2387c7c3c45dc0688
jwlee <laerad84.gmail.com>
2013-02-25 15:50:07 +0900

    Update

--- Gamma_Analysis/ClusterTimeAnalysis_Data_HeightTimeADJ.cc
+++ Gamma_Analysis/ClusterTimeAnalysis_Data_HeightTimeADJ.cc
@@ -51,6 +51,11 @@ double AdjFunc( double* x, double* par ){
   return value;
 }
 
+double AdjfuncTH( double*x ,double*par ){  
+  double value = par[0]*(-1*TMath::Log(1 + par[1])+TMath::Log( 1 + par[1]*exp( par[2]*x[0] )));
+  return value;
+}
+
 
 int main( int argc, char** argv ){
   TF1* TimeAdjFunc = new TF1("TimeAdjFunc",AdjFunc, 0, 2000,5);
@@ -58,7 +63,10 @@ int main( int argc, char** argv ){
   Double_t ParErrors[5] = {0.00245834,0.0263153,0.00188467,0.007594,4.81501e-05};
   TimeAdjFunc->SetParameters(Par);
   TimeAdjFunc->SetParErrors(ParErrors);
-  
+
+  TF1* TimeAdjFuncHeight = new TF1("TimeAdjFuncHeight",AdjfuncTH,0,16000,3);
+  TimeAdjFuncHeight->SetParameters(3.308,0.005644,0.0004385);
+
 
   std::string ROOTFILE_WAV       = std::getenv("ROOTFILE_WAV");  
   std::string ROOTFILE_GAMMACLUS = std::getenv("ROOTFILE_GAMMACLUS");
@@ -73,7 +81,26 @@ int main( int argc, char** argv ){
     ch->Add( Form("%s/run_wav_%d_Cal_FNL_COS.root",ROOTFILE_WAV.c_str(),tmpRunNumber));
   }
   GammaFinder gFinder;
-  TFile*  tfout = new TFile(Form("%s/Data_All_Cal_FNL_COS_HTADJ1.root",ROOTFILE_GAMMACLUS.c_str()),"RECREATE");
+
+  TH2Poly* poly = new  TH2Poly("poly","",-1000,1000,-1000,1000);
+  Double_t binX0[5] = {-1000,-600,-600,-1000,-1000};
+  Double_t binY0[5] = {-600,-600,600,600,-600};
+  Double_t binX1[7] = {75,-600,-600,-100,-100,75,75};
+  Double_t binY1[7] = {-600,-600,600,600,0,0,-600};
+  Double_t binX2[7] = {600,75,75,-100,-100,600,600};
+  Double_t binY2[7] = {-600,-600,0,0,600,600,-600};
+  Double_t binX3[5] = {600,1000,1000,600,600};
+  Double_t binY3[5] = {-600,-600,600,600,-600};
+  poly->AddBin( 5, binX0, binY0 );
+  poly->AddBin( 7, binX1, binY1 );
+  poly->AddBin( 7, binX2, binY2 );
+  poly->AddBin( 5, binX3, binY3 );
+  static double const Pcor[2]= {6.49003,0.99254};
+  static double const CsIX0  = 18.5;//mm
+
+
+
+  TFile*  tfout = new TFile(Form("%s/Data_All_GammaTime.root",ROOTFILE_GAMMACLUS.c_str()),"RECREATE");
   TTree*  trout = new TTree("trCluster","");
   const int arrSize = 120;
 
@@ -90,14 +117,24 @@ int main( int argc, char** argv ){
   Double_t ClusterChisq2[arrSize];//[nCluster]
   Double_t ClusterPos[arrSize][3];//[nCluster]
   Double_t GammaPos[arrSize][3];//[nCluster]
+  Double_t GammaTOF[arrSize];//[nCluster]
+  Double_t GammaDOS[arrSize];//[nCluster]
+  Int_t GammaRID[arrSize];//[nCluster]
 
   Double_t CrystalT[arrSize][arrSize]; 
+  Double_t CrystalTAdj[arrSize][arrSize]; 
   Double_t CrystalEnergy[arrSize][arrSize];
   Double_t CrystalR[arrSize][arrSize];
   Double_t CrystalPhi[arrSize][arrSize];
   Double_t CrystalSignal[arrSize][arrSize];
   Double_t CrystalHHT[arrSize][arrSize];
   Int_t    CrystalID[arrSize][arrSize];
+
+  Double_t KlongPos[3];
+  Double_t KlongEne;
+  Double_t KlongMass;
+  Double_t KlongPt;
+  Double_t KlongChisqZ;
 
   trout->Branch("EventNumber"  ,&EventNumber ,"EventNumber/I");
   trout->Branch("nCluster"     ,&nCluster    ,"nCluster/I");
@@ -110,16 +147,23 @@ int main( int argc, char** argv ){
   trout->Branch("ClusterChisq2",ClusterChisq2,"ClusterChisq2[nCluster]/D");
   trout->Branch("ClusterPhi"   ,ClusterPhi   ,"ClusterPhi[nCluster]/D");
   trout->Branch("CrystalT"     ,CrystalT     ,"CrystalT[nCluster][120]/D");
+  trout->Branch("CrystalTAdj"  ,CrystalTAdj  ,"CrystalTAdj[nCluster][120]/D");
   trout->Branch("CrystalEnergy",CrystalEnergy,"CrystalEnergy[nCluster][120]/D");
   trout->Branch("CrystalR"     ,CrystalR     ,"CrystalR[nCluster][120]/D");
   trout->Branch("CrystalPhi"   ,CrystalPhi   ,"CrystalPhi[nCluster][120]/D");
   trout->Branch("CrystalSignal",CrystalSignal,"CrystalSignal[nCluster][120]/D");
   trout->Branch("CrystalID"    ,CrystalID    ,"CrystalID[nCluster][120]/I");
   trout->Branch("ClusterPos"   ,ClusterPos   ,"ClusterPos[nCluster][3]/D");
-  trout->Branch("GammaPos"     ,GammaPos     ,"GammaPos[nCluster][3]/D");
-  
-  Double_t KlongPos[3];
-  trout->Branch("KlongPos"     ,KlongPos       ,"KlongPos[3]/D");
+  trout->Branch("GammaPos"     ,GammaPos     ,"GammaPos[nCluster][3]/D");  
+  trout->Branch("GammaTOF"     ,GammaTOF     ,"GammaTOF[nCluster]/D");
+  trout->Branch("GammaDOS"     ,GammaDOS     ,"GammaDOS[nCluster]/D");
+  trout->Branch("GammaRID"     ,GammaRID     ,"GammaRID[nCluster]/I");
+
+  trout->Branch("KlongPos"     ,KlongPos     ,"KlongPos[3]/D");
+  trout->Branch("KlongEne"     ,&KlongEne    ,"KlongEne/D");
+  trout->Branch("KlongMass"    ,&KlongMass   ,"KlongMass/D");
+  trout->Branch("KlongPt"      ,&KlongPt     ,"KlongPt/D");
+
   E14GNAnaDataContainer data;
   ClusterTimeAnalyzer* clusterTimeAnalyzer = new ClusterTimeAnalyzer();
   
@@ -150,26 +194,29 @@ int main( int argc, char** argv ){
     KlongPos[2] = 0;
 
     for( int iarr = 0; iarr < arrSize; iarr++){
-      nCrystal[iarr] = 0;
-      ClusterID[iarr] = -1;
+      nCrystal[iarr]      = 0;
+      ClusterID[iarr]     = -1;
       ClusterEnergy[iarr] = 0; 
-      ClusterR[iarr] = 0;
-      ClusterPhi[iarr] = 0;
-      ClusterT[iarr] = 0;      
-      ClusterTheta[iarr] = 0;
+      ClusterR[iarr]      = 0;
+      ClusterPhi[iarr]    = 0;
+      ClusterT[iarr]      = 0;      
+      ClusterTheta[iarr]  = 0;
       ClusterChisq2[iarr] = 0;
-      ClusterPos[iarr][0]  =0;
+      ClusterPos[iarr][0] = 0;
       ClusterPos[iarr][1] = 0;
       ClusterPos[iarr][2] = 0;
-      GammaPos[iarr][0]  =0;
-      GammaPos[iarr][1] = 0;
-      GammaPos[iarr][2] = 0;
+      GammaPos[iarr][0]   = 0;
+      GammaPos[iarr][1]   = 0;
+      GammaPos[iarr][2]   = 0;
+      GammaTOF[iarr]      = 0.;
+      GammaDOS[iarr]      = 0.;
+      GammaRID[iarr]      = 0.;
 
       for( int jarr  =0; jarr < arrSize; jarr++){
-	CrystalR[iarr][jarr] = 0;
-	CrystalT[iarr][jarr] = 0;
+	CrystalR[iarr][jarr]      = 0;
+	CrystalT[iarr][jarr]      = 0;
 	CrystalEnergy[iarr][jarr] = 0;
-	CrystalPhi[iarr][jarr] = 0;
+	CrystalPhi[iarr][jarr]    = 0;
 	CrystalSignal[iarr][jarr] = 0;
       }
     }    
@@ -181,8 +228,10 @@ int main( int argc, char** argv ){
     data.getData( clist );
     data.getData( klVec );
     gFinder.findGamma( clist, glist );
+
     if( clist.size() != 6 ){ continue; }
     if( glist.size() != 6 ){ continue; }
+    if( klVec.size() >  0 ){ continue; }
 
     //std::cout<< "Number of Cluster:" <<  clist.size() << std::endl;
 
@@ -198,6 +247,8 @@ int main( int argc, char** argv ){
     KlongPos[0] = (klVec[0]).vx();
     KlongPos[1] = (klVec[0]).vy();
     KlongPos[2] = (klVec[0]).vz();
+    KlongMass   = (klVec[0]).m();
+    KlongChisqZ = (klVec[0]).chisqZ();
 
     Int_t clIndex = 0;
     for( ;
@@ -206,7 +257,6 @@ int main( int argc, char** argv ){
       //std::cout<< (*itList).GetClusterTime() << " : " << (*itList).GetClusterR() << std::endl;
 
       ClusterID[clIndex]    = (*itCl).id();
-      ClusterT[clIndex]     = (*itlist).GetClusterTime();
       ClusterR[clIndex]     = (*itlist).GetClusterR();
       ClusterPhi[clIndex]   = (*itlist).GetClusterPhi();
       ClusterEnergy[clIndex]= (*itCl).e();
@@ -218,25 +268,66 @@ int main( int argc, char** argv ){
       GammaPos[clIndex][0]  = (*itgamma).x();
       GammaPos[clIndex][1]  = (*itgamma).y();
       GammaPos[clIndex][2]  = (*itgamma).z();
+      GammaTOF[clIndex]     = TMath::Sqrt((KlongPos[0]-GammaPos[clIndex][0])*(KlongPos[0]-GammaPos[clIndex][0])
+					  +(KlongPos[1]-GammaPos[clIndex][1])*(KlongPos[1]-GammaPos[clIndex][1])
+					  +(KlongPos[2]-GammaPos[clIndex][2])*(KlongPos[2]-GammaPos[clIndex][2]))/299.8;
+      GammaDOS[clIndex]     = CsIX0*(Pcor[0]+Pcor[1]*log(ClusterEnergy[clIndex]/1000.));//mm
+
+      ClusterT[clIndex]     = (*itlist).GetClusterTime();
+      
+      if( GammaPos[clIndex][0] < -600 ){
+	GammaRID[clIndex] = 1;
+      }else if( GammaPos[clIndex][0] > 600 ){
+	GammaRID[clIndex] = 4;
+      }else{
+	if( GammaPos[clIndex][1] < 0 ){
+	  if( GammaPos[clIndex][0] < 75 ){
+	    GammaRID[clIndex] = 2;
+	  }else{
+	    GammaRID[clIndex] = 3;
+	  }
+	}else{
+	  if( GammaPos[clIndex][0] < -100 ){
+	    GammaRID[clIndex] = 2;
+	  }else{ 
+	    GammaRID[clIndex] = 3;
+	  }
+	}
+      }
       
       nCrystal[clIndex]     = (*itCl).clusterIdVec().size();
+
+      Int_t    MaxEID      = 0;
+      Double_t MaxE        = 0;
+      Double_t MaxEHeight  = 0; 
+      Double_t MaxETime    = 0;
       for( Int_t cryIndex = 0; cryIndex < nCrystal[clIndex] ; cryIndex++ ){
 	CrystalR[clIndex][cryIndex]      = (*itlist).clusterRVec()[cryIndex];
 	CrystalPhi[clIndex][cryIndex]    = (*itlist).clusterPhiVec()[cryIndex];
 	CrystalT[clIndex][cryIndex]      = (*itlist).clusterTimeDeltaVec()[cryIndex];
+
 	CrystalEnergy[clIndex][cryIndex] = (*itCl).clusterEVec()[cryIndex];
 	CrystalID[clIndex][cryIndex]     = (*itlist).clusterIDVec()[cryIndex];
+	
 	for( int idigi = 0; idigi < CsiNumber; idigi++){
-	  if( CrystalID[clIndex][cryIndex] == CsiDigiID[idigi] ){
-	    CrystalSignal[clIndex][cryIndex] = CsiSignal[idigi];
+	  if( CrystalID[clIndex][cryIndex]   == CsiDigiID[idigi] ){
+	    CrystalSignal[clIndex][cryIndex] =  CsiSignal[idigi];
 	    break;
 	  }
 	}
+
+	CrystalTAdj[clIndex][cryIndex] = (*itlist).clusterTimeDeltaVec()[cryIndex] - TimeAdjFuncHeight->Eval(CrystalSignal[clIndex][cryIndex]);
+	/// Set Maximum Energy deposited Crystal Time as ClusterTime;
+	if( MaxE < CrystalEnergy[clIndex][cryIndex] ){
+	  MaxE     = CrystalEnergy[clIndex][cryIndex];
+	  MaxETime = (*itCl).clusterTimeVec()[cryIndex] -TimeAdjFuncHeight->Eval(CrystalSignal[clIndex][cryIndex]);
+	}	
       }
+      ClusterT[clIndex] = MaxETime;
     }
     trout->Fill();
   }
-  
+
   trout->Write();
   tfout->Close();
 }
--- Gamma_Analysis/Draw_GammaClusterTime_New.cc
+++ Gamma_Analysis/Draw_GammaClusterTime_New.cc
@@ -11,6 +11,26 @@
 #include "TProfile2D.h"
 #include "TProfile.h"
 #include "CsIPoly.h"
+#include "TH2Poly.h"
+
+
+#include "gnana/DigiReader.h"
+#include "gnana/E14GNAnaFunction.h"
+#include "gnana/E14GNAnaDataContainer.h"
+#include "klong/Klong.h"
+#include "pi0/Pi0.h"
+#include "gamma/Gamma.h"
+#include "cluster/Cluster.h"
+
+#include "gamma/GammaFinder.h"
+#include "cluster/ClusterFinder.h"
+
+#include "rec2g/Rec2g.h"
+#include "CLHEP/Vector/ThreeVector.h"
+//#include "User_Function.h"
+#include "ClusterTimeStructure.h"
+
+
 
 double AdjFunc( double* x, double* par ){
   double x0 = x[0];
@@ -61,7 +81,22 @@ int main( int argc, char** argv) {
 
   TF1* TimeAdjFuncHeight = new TF1("TimeAdjFuncHeight",AdjfuncTH,0,16000,3);
   TimeAdjFuncHeight->SetParameters(3.308,0.005644,0.0004385);
-
+ 
+  TH2Poly* poly = new  TH2Poly("poly","",-1000,1000,-1000,1000);
+  Double_t binX0[5] = {-1000,-600,-600,-1000,-1000};
+  Double_t binY0[5] = {-600,-600,600,600,-600};
+  Double_t binX1[7] = {75,-600,-600,-100,-100,75,75};
+  Double_t binY1[7] = {-600,-600,600,600,0,0,-600};
+  Double_t binX2[7] = {600,75,75,-100,-100,600,600};
+  Double_t binY2[7] = {-600,-600,0,0,600,600,-600};
+  Double_t binX3[5] = {600,1000,1000,600,600};
+  Double_t binY3[5] = {-600,-600,600,600,-600};
+  poly->AddBin( 5, binX0, binY0 );
+  poly->AddBin( 7, binX1, binY1 );
+  poly->AddBin( 7, binX2, binY2 );
+  poly->AddBin( 5, binX3, binY3 );
+  static double const Pcor[2]= {6.49003,0.99254};
+  static double const CsIX0  = 18.5;//mm
 
   TChain* ch = new TChain("T");
   std::ifstream ifs(Form("%s/Data/RunList/KLRunList_2.txt",ANALYSISLIB.c_str()));
@@ -70,104 +105,35 @@ int main( int argc, char** argv) {
     std::cout<<  tmpRunNumber  << std::endl;
     ch->Add(Form("%s/run_wav_%d_Cal_FNL_COS.root", ROOTFILE_WAV.c_str(),tmpRunNumber));
   }
+  E14GNAnaDataContainer data;
+  data.setBranchAddress(ch);
+
+
 
   GammaFinder gFinder;
-
-
-  Int_t EventNumber;
-  Int_t RunNumber;
-  Int_t nCrystal;
-  Int_t ClusterID;
-  
-  E14GNAnaDataContainer data;
   std::cout<< ch->GetEntries() << std::endl;
-  for( Int_t eventIndex = 0; eventIndex  < ch->GetEntries(); eventIndex++){
-    ch->GetEntry( eventIndex );
-    EventNumber = eventIndex;
-
-    std::list<Cluster> clist;
-    std::list<Gammma>  glist;
-    std::vector<Klong> klVec;
-    
-    data.getData( clist );
-    data.getData( klVec );
-    gFinder.findGamma( clist, glist );
-    if( clist.size() != 6 ){ continue; }
-    if( glist.size() != 6 ){ continue; }
-
-    std::list<Cluster>::iterator itcl;
-
-  }
-
-  
 
 
-  /*
-  double ene = gam.e();  
-  double sinTheta = sin(gam.p3().theta());
-  double newx = gam.coex()-L*sinTheta*cos(gam.p3().phi());
-  double newy = gam.coey()-L*sinTheta*sin(gam.p3().phi());
-  */
 
-  static double const Pcor[2]= {6.49003,0.99254};
-  static double const CsIX0  = 18.5;//mm
 
-  for( int ievent = 0; ievent < nEntries; ievent++){
-    reader->GetEntry(ievent);
-    hisCut->Fill(0);
-    Int_t    MEGIndex    = -1;
-    Double_t MEGEnergy   = 0;
-    Double_t MEGTime     = 0;
-    Double_t MEGTimeNon  = 0;
-    Double_t MEGTimeNew  = 0;
-    Double_t MEGR        = 1000;
-    Double_t GTime[6]    = {0.};
-    Double_t GRad[6]     = {0.};    
-    Double_t GTimeNon[6] = {0.};
-    Double_t GTimeNew[6] = {0.};
-    Double_t GPhi[6]    = {0.};
-    Double_t GE[6]      = {0.};
-    Double_t GCE[6]     = {0.};
-    Double_t GCT[6]     = {0.};
-    Double_t GL[6]      = {0.};
-    if( reader->nCluster !=6 ){ continue; }
-    bool ClusterRegion = true;
-    bool ClusterMinRBool = false;//  < 300 mm at least one crystal 
 
-    for( int clusterIndex = 0; clusterIndex < reader->nCluster; clusterIndex++){
-      Double_t clusterR      = reader->ClusterR[clusterIndex];
-      Double_t clusterTheta  = reader->ClusterTheta[clusterIndex];
-      Double_t clusterPhi    = reader->ClusterPhi[clusterIndex];
-      Double_t clusterEnergy = reader->ClusterEnergy[clusterIndex];
-      Double_t clusterT      = reader->ClusterT[clusterIndex];
-      Double_t clusterZ      = clusterR/TMath::Tan(clusterTheta);
-      Double_t clusterL      = clusterR/TMath::Sin(clusterTheta);
-      Double_t MECHeight     = 0;
-      Double_t MECEnergy     = 0;      
-      
-      Double_t clusterx      = reader->ClusterPos[clusterIndex][0];
-      Double_t clustery      = reader->ClusterPos[clusterIndex][1];
-      Double_t clusterz      = reader->ClusterPos[clusterIndex][2];
-      Double_t klongz        = reader->KlongPos[2];
+  TFile* tfOut = new TFile("GammaTimeOut.root","recreate");
+  TTree* trOut = new TTree("trOut","");
 
-      Double_t Length        = sqrt( (klongz-clusterz)*(klongz-clusterz)+ clusterx*clusterx+clustery*clustery);
-      GRad[clusterIndex] = clusterR;
-      GL[clusterIndex]   = Length;
-      GTime[clusterIndex]   = clusterT;
-      GE[clusterIndex]   = clusterEnergy;
-    }    
-    for( int i = 0; i< 6; i++){
-      for( int j = i+1; j< 6; j++){
-	hisRT->Fill(GRad[i],GTime[i]-GL[i]/299.8-(GTime[j]-GL[i]/299.8));
-      }
-    }
-  }
-  hisWT->Write();
-  hisHT->Write();
-  profTime->Write();
-  hisCut->Write();
-  hisRTNon->Write();
-  hisRTNew->Write();
-  hisRT->Write();
-  tfout->Close();
+  Int_t    GammaRegion[6];
+  Int_t    GammaCrystalID[6];
+  Double_t GammaEnergy[6];
+  Double_t GammaTime[6];
+  Double_t GammaTimeV0[6];
+  Double_t GammaPos[6][3];
+  Double_t GammaTheta[6];
+  Double_t LengthOfFlight[6];
+  Double_t DepthOfShower[6];
+
+  Double_t KlongPos[3];
+  Double_t KlongEne;
+  Double_t KlongMass;
+  Double_t KlongPt;
+  Double_t KlongChisqZ;
+
 }
