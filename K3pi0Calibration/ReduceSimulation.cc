1f84ba9377e8644ac1c72d851db7808a39e18003
jwlee <laerad84.gmail.com>
2013-04-16 22:33:50 +0900

    Update

--- K3pi0Calibration/ReduceSimulation.cc
+++ K3pi0Calibration/ReduceSimulation.cc
@@ -9,17 +9,21 @@
 #include <cstdlib>
 #include <cstdio>
 int main( int argc, char** argv ){
-  int Number = std::atoi( argv[1] );
-  std::cout<< "RunNumber :" << Number << std::endl;
+  //int Number = std::atoi( argv[1] );
+  //std::cout<< "RunNumber :" << Number << std::endl;  
+  //std::string ROOTFILE_GSIM = "";  
+  //TFile* tfReduced = new TFile(Form("%s/Out_KL3pi0Fast_Reduced_1E7_%d.root",ROOTFILE_GSIM.c_str(),Number),"recreate");
+  std::ofstream ofs(argv[3]);
   
-  std::string ROOTFILE_GSIM = "";
-  
-  TFile* tfReduced = new TFile(Form("%s/Out_KL3pi0Fast_Reduced_1E7_%d.root",ROOTFILE_GSIM.c_str(),Number),"recreate");
+  TFile* tfReduced = new TFile(argv[2],"recreate");
   TTree* trC;
-  TFile* tf = new TFile(Form("%s/Out_KL3pi0Fast.mac_10000000_%d.root",ROOTFILE_GSIM.c_str(),Number));
+  TFile* tf = new TFile(argv[1]);
   TTree* tr = (TTree*)tf->Get("eventTree00");
   tfReduced->cd();
   trC = tr->CopyTree("GenParticle.briefTracks.end_v.fZ[2]==6148&&GenParticle.briefTracks.end_v.fZ[3]==6148&&GenParticle.briefTracks.end_v.fZ[5]==6148&&GenParticle.briefTracks.end_v.fZ[6]==6148&&GenParticle.briefTracks.end_v.fZ[8]==6148&&GenParticle.briefTracks.end_v.fZ[9]==6148");
+  Int_t nEntries = trC->GetEntries();
+  ofs << nEntries << std::endl;
+  ofs.close();
   trC->Write();  
   tfReduced->Close();
 }
