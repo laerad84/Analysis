1d22f956dd587bdfb41009e2ac0cb13c057a32a3
jwlee <laerad84.gmail.com>
2013-09-16 13:31:55 +0900

    Update

--- K3pi0Producer/K3pi0ClusterTimeDelta_SIM_FNL_KLCONV_RES_LY_SUGAR.cc
+++ K3pi0Producer/K3pi0ClusterTimeDelta_SIM_FNL_KLCONV_RES_LY_SUGAR.cc
@@ -106,7 +106,7 @@ main( int argc ,char ** argv ){
   */
 
   TChain* trin = new TChain("Tree");
-  for( int i = RunNumber*100; i < (RunNumber+1)*100; i++){
+  for( int i = RunNumber*50; i < (RunNumber+1)*50; i++){
     trin->Add(Form(iFileForm.c_str(),ROOTFILE_SIM3PI0.c_str(),i));
   }
   int    RunNo;
