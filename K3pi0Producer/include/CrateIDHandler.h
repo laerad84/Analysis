e41bf16690b5973bec7176393454787e88d43404
jwlee <laerad84@gmail.com>
2014-01-29 22:26:23 +0900

    update

---
+++ K3pi0Producer/include/CrateIDHandler.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include "TROOT.h"
+
+class CrateIDHandler {
+ public:
+  CrateIDHandler();
+  ~CrateIDHandler();
+  Short_t Crate[2716];
+  Short_t Slot[2716];
+  Short_t Channel[2716];
+  Short_t L1[2716];
+
+  void Set();
+  Short_t GetCrate( int id );
+  Short_t GetSlot( int id );
+  Short_t GetChannel( int id );
+  Short_t GetL1( int id );
+
+};
