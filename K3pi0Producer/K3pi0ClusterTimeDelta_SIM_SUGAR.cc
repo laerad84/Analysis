8c79829bce72578655929766e05c165ade5b63a6
jwlee <laerad84@gmail.com>
2014-02-19 23:10:53 +0900

    Update

--- K3pi0Producer/K3pi0ClusterTimeDelta_SIM_SUGAR.cc
+++ K3pi0Producer/K3pi0ClusterTimeDelta_SIM_SUGAR.cc
@@ -61,12 +61,11 @@ main( int argc ,char ** argv ){
   //std::string oFileForm        = "%s/Sim3pi0_wav_fast_5E6_%d_Calibration.root";     //ROOTFILE_SIM3PI0
   //std::string oFileForm        = "%s/Sim3pi0_wav_fast_5E6_%d_Calibration_mis_1.root";     //ROOTFILE_SIM3PI0
 
-  //std::string iFileForm        = "%s/Conv_KL3pi0_ALCV_5000000_%d.root"; //ROOTFILE_SIMCONV
-  //std::string oFileForm        = "%s/Sim3pi0_wav_ALCV_5E8_%d.root";     //ROOTFILE_SIM3PI0
+  std::string iFileForm        = "%s/Conv_KL3pi0_ALCV_5000000_%d.root"; //ROOTFILE_SIMCONV
+  std::string oFileForm        = "%s/Sim3pi0_wav_ALCV_5E8_NOADJ_%d.root";     //ROOTFILE_SIM3PI0, No Cal constant Adj
 
-
-  std::string iFileForm        = "%s/Conv_e14_KL3pi0_1000000_%d.root";
-  std::string oFileForm        = "%s/Sim_e14_KL3pi0_1000000_%d.root";
+  //std::string iFileForm        = "%s/Conv_e14_KL3pi0_1000000_%d.root";
+  //std::string oFileForm        = "%s/Sim_e14_KL3pi0_1000000_%d.root";
 
   //std::string TCalFile = Form("%s/Data/TimeOffset/TimeOffset_with_cosmic.dat",ANALYSISLIB.c_str());  
   std::string TCalFile = Form("%s/Data/TimeOffset/testNewWORKCompileOffset.txt",ANALYSISLIB.c_str());  
@@ -238,7 +237,7 @@ main( int argc ,char ** argv ){
       double tmpEne = CsiEne[ich];
       if( tmpSignal > 5 && tmpEne > 0.5 ){
 	CSIDigiID[nCSIDigi]     = tmpID;
-	CSIDigiE[nCSIDigi]      = tmpEne/CalibrationFactor[CsiModID[ich]];
+	CSIDigiE[nCSIDigi]      = tmpEne;//CalibrationFactor[CsiModID[ich]];//20140219
 	//CSIDigiE[nCSIDigi]      = tmpEne/CalibrationFactor[CsiModID[ich]]*(1+0.02*(tmpSignal/10000)*(tmpSignal/10000));// For Calibration Reconstruction Test.
 	//CSIDigiE[nCSIDigi]      = tmpEne/CalibrationFactor[CsiModID[ich]]*(1+0.02*(tmpSignal/10000)*(tmpSignal/10000))*(1.013-9.953e-3*exp(tmpSignal/1.5314e4));// First Adjustment
 	CSIDigiSignal[nCSIDigi] = tmpSignal;
