f5e2795b0fb003b64eb43c24277acd3de39be068
jwlee <laerad84@gmail.com>
2014-02-19 23:31:06 +0900

    Update

--- K3pi0Producer/K3pi0ClusterTimeDelta_SIM_FNL_KLCONV_RES_LY_SUGAR.cc
+++ K3pi0Producer/K3pi0ClusterTimeDelta_SIM_FNL_KLCONV_RES_LY_SUGAR.cc
@@ -91,8 +91,8 @@ main( int argc ,char ** argv ){
   //std::string oFileForm          = "%s/Sim_e14_KL3pi0_KL_RES_LY_pe_1E8_%d.root"; // ROOTFILE_SIM3PI0
   //std::string oFileForm          = "%s/Sim_e14_KL3pi0_KL_RES_LY_pe_1E8_NON10_%d.root"; // ROOTFILE_SIM3PI0
 
-  std::string iFileForm          = "%s/Sim3pi0_wav_ALCV_5E8_%d.root";    //ROOTFILE_SIM3PI0
-  std::string oFileForm          = "%s/Sim3pi0_wav_ALCV_KL_RES_LY_pe_5E8KL_NONLIN_%d.root"; // ROOTFILE_SIM3PI0
+  std::string iFileForm          = "%s/Sim3pi0_wav_ALCV_5E8_NOADJ_%d.root";    //ROOTFILE_SIM3PI0
+  std::string oFileForm          = "%s/Sim3pi0_wav_ALCV_KL_RES_LY_pe_5E8KL_%d.root"; // ROOTFILE_SIM3PI0
 
   //std::string iFileForm = "%s/Sim3pi0_wav_fast_5E6_%d_Calibration.root";
   //std::string oFileForm = "%s/Sim3pi0_wav_fast_KL_RES_LY_pe_5E6_%d_Calibration_%s.root";
@@ -277,7 +277,7 @@ main( int argc ,char ** argv ){
 	CSIDigiID[nCSIDigi]     = tmpID;
 	CSIDigiE[nCSIDigi]      = (tmpEne);
 	//CSIDigiE[nCSIDigi]      = NonLinFunction_1(tmpEne)*mont;
-	CSIDigiSignal[nCSIDigi] = tmpSignal*mont;
+	CSIDigiSignal[nCSIDigi] = tmpSignal;//*mont;
 	CSIDigiTime[nCSIDigi]   = tmpTime;
 	CSIDigiHHTime[nCSIDigi] = tmpHHTime;
 	nCSIDigi++;
