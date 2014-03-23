c82070843b0dba912814cafefa148c6cd66f2838
jwlee <laerad84@gmail.com>
2014-03-24 08:12:31 +0900

    update

--- K3pi0Producer/K3pi0ClusterTimeDelta_GammaTime.cc
+++ K3pi0Producer/K3pi0ClusterTimeDelta_GammaTime.cc
@@ -55,7 +55,7 @@ main( int argc ,char ** argv ){
   std::string HOME         = std::getenv("HOME");
 
   std::string iFileForm="%s/run_wav_%d.root";
-  std::string oFileForm="%s/run_wav_%d_GammaTime_10ns.root";
+  std::string oFileForm="%s/run_wav_%d_GammaTime_10nsCut.root";
   const int nGammaCut = 6;
   std::string TCalFile = Form("%s/Data/TimeOffset/testNewWORKCompileOffset.txt",ANALYSISLIB.c_str());  
   //std::string TCalFile = Form("%s/Data/CalibrationFile/TimeOffset_ShowerHeight_10.dat",ANALYSISLIB.c_str());  
@@ -542,11 +542,11 @@ main( int argc ,char ** argv ){
     */ 
     if( glist.size() < 6 ){ continue; }
     //if( glistTCut.size() != nGammaCut ){ continue; }
-    if( glist.size() == nGammaCut ){
-      if( user_rec(glist,klVec)){
-	gammaCut->Decision( glist );
+    if( glistTCut.size() == nGammaCut ){
+      if( user_rec(glistTCut,klVec)){
+	gammaCut->Decision( glistTCut );
 	data.setData( clist );
-	data.setData( glist );
+	data.setData( glistTCut );
 	user_cut( data, klVec );
 	data.setData(klVec);    
 	std::list<Gamma>::iterator git = glist.begin();
